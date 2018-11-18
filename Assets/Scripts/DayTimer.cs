7424337341daa94cc9493bbee5cec9be4fe7d344
Jonathan <jgregfong@gmail.com>
2018-11-17 19:42:00 -0800

    Added a timer prefab with scripts.

---
+++ Assets/Scripts/DayTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayTimer : MonoBehaviour {
+    Image fillImg;
+    public float timeAmount = 20f;
+    float time;
+    public Text timeText;
+
+	// Use this for initialization
+	void Start () {
+        fillImg = this.GetComponent<Image>();
+        time = timeAmount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            fillImg.fillAmount = time / timeAmount;
+            timeText.text = "Time: " + time.ToString("f");
+        }
+		
+	}
+}
