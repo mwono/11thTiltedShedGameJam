cb77fac21c4dbc1fe1dc4fffe6e27870bcfd323f
mwono <michaelwono@gmail.com>
2018-11-17 17:33:38 -0800

    Added audios sfx

--- Assets/Scripts/NonplayerEnemies.cs
+++ Assets/Scripts/NonplayerEnemies.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flowers : MonoBehaviour {
+public class NonplayerEntities : MonoBehaviour {
     public GameObject UI;
     public int score;
 
